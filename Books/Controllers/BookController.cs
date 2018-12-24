da1f7f755a615a0b365f088c9a2682dee9f79346
TRDOM\esekerci <esekerci@trabilisim.com>
2018-12-25 00:28:16 +0300

    project created

---
+++ Books/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Books.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Read
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Navigation()
+        {
+            return View();
+        }
+
+        public ActionResult welcome()
+        {
+            return View();
+        }
+    }
+}
